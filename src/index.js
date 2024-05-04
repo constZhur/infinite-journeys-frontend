7cbf39981898939bf23224259986f8f22bbdad79
Konstantin Zhuravlev <113041604+constZhur@users.noreply.github.com>
2024-05-04 15:06:14 +0300

    Добавлены формы регистариции и авторизации

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
+import AppStore from "./store/AppStore";
+import {ConfigProvider, theme} from "antd";
+import ruRU from "antd/lib/locale/ru_RU";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = new AppStore();
+
+export const Context = createContext({
+    store,
+})
+
+root.render(
+    <React.StrictMode>
+        <Context.Provider value={{store}}>
+            <ConfigProvider theme={{algorithm: theme.darkAlgorithm}} locale={ruRU}>
+                <BrowserRouter>
+                    <App/>
+                </BrowserRouter>
+            </ConfigProvider>
+        </Context.Provider>
+    </React.StrictMode>
+);
